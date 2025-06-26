d0154a69f160f8ef852eb67b77d5f6528d9daa94
pengxy2024 <pengxy2024@shanghaitech.edu.cn>
2025-06-26 11:01:17 +0800

    add a simple tensorboard logger

--- src/algorithms/online_models/random.py
+++ src/algorithms/online_models/random.py
@@ -3,25 +3,29 @@ from src.datasets.prompt_only import PromptOnlyDataset
 from torch.utils.data import DataLoader
 import numpy as np
 from src.metrics.table_base import TabelBasedModel
+from src.logger import Logger
 
 simuler_dataset = SimulerDataset(file_path="data/processed/all-mpnet-base-v2/routerbench_0shot/train.parquet")
 
 dataset = PromptOnlyDataset(simuler_dataset)
 env_model = TabelBasedModel(simuler_dataset)
+logger = Logger("./outputs/logs/random/")
 
 for t, data in enumerate(dataset):
-    print(f"========== step {t} ===============")
+    # print(f"========== step {t} ===============")
     x = data["prompt"]
-    print(f"PROMPT: {x}")
+    # print(f"PROMPT: {x}")
     avilable_models = list(data["available_models_description"].keys())
 
     a = np.random.choice(avilable_models)
-    print("CHOOSED LM:", a)
+    # print("CHOOSED LM:", a)
     response, reward, cost = env_model.feedback(x, a)
-    print(f"RESPONSE: {response}")
-    print(f"REWARD: {reward}")
-    print(f"COST: {cost}")
-    if t > 10:
-        break  # Remove this line to process all batches
+    logger.log_scalar(
+        {
+            "train/reward": reward,
+            "train/cost": cost,
+        },
+        step=t,
+    )
 
 
---
+++ src/logger.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+import os
+from typing import TYPE_CHECKING, Any, ClassVar
+from typing_extensions import Self  # Python 3.11+
+
+import torch.utils.tensorboard as tensorboard
+
+class Logger:
+
+    _instance: ClassVar[Logger] = None  # singleton pattern
+    writer: tensorboard.SummaryWriter
+
+    def __new__(
+            cls, 
+            log_dir: str | os.PathLike | None = None,
+    ):
+        if cls._instance is None:
+            self = cls._instance = super().__new__(
+                cls,
+            )
+            self.writer = tensorboard.SummaryWriter(log_dir=log_dir)
+        else:
+            assert log_dir is None, "Cannot change log_dir after Logger is initialized"
+        return cls._instance
+    
+    def log_scalar(self, metrics: dict[str, Any], step: int = 0):
+        """
+        Log metrics to TensorBoard.
+        
+        Args:
+            metrics (dict): Dictionary of metrics to log.
+            step (int): Global step value to record.
+        """
+        for key, value in metrics.items():
+            self.writer.add_scalar(key, value, step)
+
+
